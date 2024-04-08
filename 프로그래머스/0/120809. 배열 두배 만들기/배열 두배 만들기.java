ddeed2cf21f4d634b46790ffc88ff712d83c8916
강민혁 <96116158+kangminhyuk1111@users.noreply.github.com>
2024-04-08 16:50:38 +0900

    [level 0] Title: 배열 두배 만들기, Time: 0.03 ms, Memory: 75 MB -BaekjoonHub

--- 프로그래머스/0/120809. 배열 두배 만들기/배열 두배 만들기.java
+++ 프로그래머스/0/120809. 배열 두배 만들기/배열 두배 만들기.java
@@ -1,19 +1,12 @@
-import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.*;
 
 class Solution {
     public int[] solution(int[] numbers) {
-        ArrayList<Integer> answerList = new ArrayList<>();
-        for (int number : numbers) {
-            answerList.add(number * 2);
+        int[] answer = {};
+        answer = new int[numbers.length];
+        for(int i=0; i<answer.length; i++){
+            answer[i] = numbers[i]*2;
         }
-
-        // ArrayList를 int[] 배열로 변환
-        int[] answerArray = new int[answerList.size()];
-        for (int i = 0; i < answerList.size(); i++) {
-            answerArray[i] = answerList.get(i);
-        }
-
-        return answerArray;
+        return answer;
     }
 }
