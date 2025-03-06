5e86c08615876dd2fc9d17271da4a45e30e61432
Minhyuk Kang <96116158+kangminhyuk1111@users.noreply.github.com>
2025-03-06 13:53:03 +0900

    [level 1] Title: 폰켓몬, Time: 11.08 ms, Memory: 72.8 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -1,15 +1,11 @@
-import java.util.*;
+import java.util.Arrays;
+import java.util.stream.Collectors;
 
 class Solution {
     public int solution(int[] nums) {
-        int max = nums.length / 2;
-        
-        Map<Integer, Integer> pokemons = new HashMap();
-        
-        for(int num: nums) {
-            pokemons.put(num, pokemons.getOrDefault(num, 0) + 1);
-        }
-        
-        return Math.min(pokemons.size(), max);
+        return Arrays.stream(nums)
+            .boxed()
+            .collect(Collectors.collectingAndThen(Collectors.toSet(),
+            pokemons -> Integer.min(pokemons.size(), nums.length / 2)));
     }
 }
