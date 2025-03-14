0b77ff33d177e6be3f981c371096b8b18b771eb9
Minhyuk Kang <96116158+kangminhyuk1111@users.noreply.github.com>
2025-03-14 14:31:04 +0900

    [Silver V] Title: 너의 평점은, Time: 108 ms, Memory: 14464 KB -BaekjoonHub

--- 백준/Silver/25206. 너의 평점은/너의 평점은.java
+++ 백준/Silver/25206. 너의 평점은/너의 평점은.java
@@ -1,50 +1,51 @@
-import java.util.Scanner;
+import java.io.*;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Main {
-	public static void main(String[] args){
-		Scanner sc = new Scanner(System.in);
-
-		double result = 0, credit_sum = 0; // result = 학점*과목평균, credit_sum = 학점의 총합
-		char c;
-				
-		for(int i=0;i<20;i++) {
-			double credit = 0; // 학점
-			double grade = 0; // 등급에 따른 점수
-			
-			String str_arr[] = sc.nextLine().split(" ");
-			credit += Double.parseDouble(str_arr[1]);
-			c = str_arr[2].charAt(0);
-			
-			switch(c) {
-			case 'A':
-				if(str_arr[2].charAt(1) == '+') grade += 4.5;
-				else grade += 4.0;
-				break;
-			case 'B':
-				if(str_arr[2].charAt(1) == '+') grade += 3.5;
-				else grade += 3.0;
-				break;
-			case 'C':
-				if(str_arr[2].charAt(1) == '+') grade += 2.5;
-				else grade += 2.0;
-				break;
-			case 'D':
-				if(str_arr[2].charAt(1) == '+') grade += 1.5;
-				else grade += 1.0;
-				break;
-			case 'F':
-				grade += 0;
-				break;
-			case 'P':
-				grade += 0;
-				credit = 0;
-				break;
-			}
-			credit_sum += credit;
-			result += credit*grade;
-		}
-		sc.close();
-
-		System.out.println(result/credit_sum);
-	}
+    public static void main(String args[]) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        Map<String, Double> rank = new HashMap<>();
+        setUpRank(rank);
+        double totalGradePoints = 0;
+        double totalCredits = 0;
+        
+        for(int i = 0; i < 20; i++) {
+            String[] inputs = br.readLine().split(" ");
+            String subjectName = inputs[0];
+            double credit = Double.parseDouble(inputs[1]);
+            String grade = inputs[2];
+            
+            // P 등급은 계산에서 제외
+            if (grade.equals("P")) {
+                continue;
+            }
+            
+            Double gradePoint = rank.get(grade);
+            totalGradePoints += credit * gradePoint;
+            totalCredits += credit;
+        }
+        
+        // 전공평점 계산
+        double majorGPA = totalGradePoints / totalCredits;
+        
+        bw.write(String.valueOf(majorGPA));
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+    
+    public static void setUpRank(Map<String, Double> rank) {
+        rank.put("A+", 4.5);
+        rank.put("A0", 4.0);
+        rank.put("B+", 3.5);
+        rank.put("B0", 3.0);
+        rank.put("C+", 2.5);
+        rank.put("C0", 2.0);
+        rank.put("D+", 1.5);
+        rank.put("D0", 1.0);
+        rank.put("F", 0.0);
+    }
 }
