4130dec9ba17acdc1fa8861e7314421aa498bea6
Minhyuk Kang <96116158+kangminhyuk1111@users.noreply.github.com>
2025-03-20 09:31:59 +0900

    [Silver III] Title: 블로그, Time: 308 ms, Memory: 35244 KB -BaekjoonHub

---
+++ 백준/Silver/21921. 블로그/블로그.java
@@ -0,0 +1,46 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        int N = Integer.parseInt(st.nextToken());
+        int X = Integer.parseInt(st.nextToken());
+        int[] visitors = new int[N];
+        st = new StringTokenizer(br.readLine(), " ");
+        for (int i = 0; i < N; i++) {
+            visitors[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int sum = 0;
+        for(int i = 0; i < X; i++) {
+            sum += visitors[i];
+        }
+        
+        int max = sum;
+        int answer = 1;
+        for(int i = 0; i < N - X; i++) {
+            sum += visitors[i+X];
+            sum -= visitors[i];
+            
+            if(sum == max) {
+                answer++;
+            }
+            
+            if(sum > max) {
+                max = sum;
+                answer = 1;
+            }
+        }
+        
+        if (max == 0) {
+            System.out.println("SAD");
+        } else {
+            System.out.println(max);
+            System.out.println(answer);
+        }
+    }
+}
