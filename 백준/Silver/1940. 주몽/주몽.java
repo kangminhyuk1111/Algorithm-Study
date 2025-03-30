9c41fb6a23ca66716bd391d398093e5f87939be6
Minhyuk Kang <96116158+kangminhyuk1111@users.noreply.github.com>
2025-03-30 15:08:27 +0900

    [Silver IV] Title: 주몽, Time: 176 ms, Memory: 17496 KB -BaekjoonHub

--- 백준/Silver/1940. 주몽/주몽.java
+++ 백준/Silver/1940. 주몽/주몽.java
@@ -1,44 +1,33 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.Arrays;
-import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
-        int N = Integer.parseInt(stringTokenizer.nextToken());
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        stringTokenizer = new StringTokenizer(bufferedReader.readLine());
-        int M = Integer.parseInt(stringTokenizer.nextToken());
+        int N = Integer.parseInt(br.readLine());
+        int M = Integer.parseInt(br.readLine());
+        final int[] items = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).sorted().toArray();
 
-        int[] arr = new int[N+1];
+        int i = 0;
+        int j = N-1;
+        int result = 0;
 
-        stringTokenizer = new StringTokenizer(bufferedReader.readLine());
-
-        for(int i = 1; i <= N; i++){
-            arr[i] += Integer.parseInt(stringTokenizer.nextToken());
-        }
-
-        Arrays.sort(arr);
-
-        int startIndex = 1; // 1
-        int endIndex = N; // 6
-        int count = 0;
-
-        while (startIndex != endIndex){
-            if(arr[startIndex] + arr[endIndex] == M){
-                count++;
-                startIndex--;
-                endIndex--;
-            } else if (arr[startIndex] + arr[endIndex] < M){
-                startIndex++;
-            } else if (arr[startIndex] + arr[endIndex] > M){
-                endIndex--;
+        while(i < j) {
+            if(items[i] + items[j] < M) {i++;}
+            else if(items[i] + items[j] > M) {j--;}
+            else {
+                j--;
+                i++;
+                result++;
             }
         }
 
-        System.out.println(count);
+        bw.write(String.valueOf(result));
+
+        bw.flush();
+        bw.close();
+        br.close();
     }
 }
