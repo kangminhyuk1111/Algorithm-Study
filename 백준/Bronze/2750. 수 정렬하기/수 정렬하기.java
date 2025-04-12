7d2b6e21eaa1a35dd1a1d5241ccd366d79b089a0
Minhyuk Kang <96116158+kangminhyuk1111@users.noreply.github.com>
2025-04-12 17:36:15 +0900

    [Bronze II] Title: 수 정렬하기, Time: 144 ms, Memory: 16120 KB -BaekjoonHub

--- 백준/Bronze/2750. 수 정렬하기/수 정렬하기.java
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.java
@@ -1,5 +1,4 @@
 import java.io.*;
-import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -14,15 +13,8 @@ public class Main {
             arr[i] = item;
         }
 
-        for (int j = 0; j < arr.length; j++) {
-            for (int i = 0; i < arr.length - 1; i++) {
-                if(arr[i] > arr[i+1]) {
-                    int temp = arr[i];
-                    arr[i] = arr[i+1];
-                    arr[i+1] = temp;
-                }
-            }
-        }
+        // bubbleSort(arr);
+        selectionSort(arr);
 
         for (int i = 0; i < N; i++) {
             bw.write(arr[i] + "\n");
@@ -31,5 +23,35 @@ public class Main {
         bw.flush();
         bw.close();
         br.close();
+    }
+
+    private static void bubbleSort(final int[] arr) {
+        for (int j = 0; j < arr.length; j++) {
+            for (int i = j; i < arr.length - 1; i++) {
+                if(arr[i] > arr[i+1]) {
+                    int temp = arr[i];
+                    arr[i] = arr[i+1];
+                    arr[i+1] = temp;
+                }
+            }
+        }
+    }
+
+    private static void selectionSort(final int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            int minIndex = i;  // 최소값의 인덱스를 저장
+
+            // 최소값 찾기
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j;
+                }
+            }
+
+            // 최소값을 현재 위치와 교환
+            int temp = arr[i];
+            arr[i] = arr[minIndex];
+            arr[minIndex] = temp;
+        }
     }
 }
